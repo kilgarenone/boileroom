4b565f9175a2de8fc53d03d24ed490cd13c08c77
Kheoh Yee Wei <kwei88@gmail.com>
2020-04-29 17:12:00 +0800

    Update webpack.prod.js

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,5 +1,6 @@
 const webpack = require("webpack");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
@@ -27,30 +28,32 @@ const configureBabelLoader = (browserList = []) => {
           [
             "@babel/preset-env",
             {
-              exclude: ["transform-regenerator", "transform-async-to-generator"],
+              exclude: [
+                "transform-regenerator",
+                "transform-async-to-generator",
+              ],
               debug: true,
               modules: false, // don't transpile into CommonJS. crucial for tree-shaking
               corejs: 3,
               useBuiltIns: "usage",
               targets: {
-                browsers: browserList
-              }
-            }
-          ]
+                browsers: browserList,
+              },
+            },
+          ],
         ],
         plugins: [
           [
             "@babel/plugin-transform-react-jsx",
             {
-              pragma: "h"
-            }
+              pragma: "h",
+            },
           ],
           "@babel/proposal-class-properties",
-          "@babel/plugin-syntax-dynamic-import",
-          "module:fast-async"
-        ]
-      }
-    }
+          "module:fast-async",
+        ],
+      },
+    },
   };
 };
 // Configure Compression webpack plugin with zopfli
@@ -61,11 +64,11 @@ const configureCompression = () => {
     minRatio: 0.8, // only process files when (Compressed Size / Original Size) < minRatio
     compressionOptions: {
       numiterations: 15,
-      level: 9
+      level: 9,
     },
     algorithm(input, compressionOptions, callback) {
       return zopfli.gzip(input, compressionOptions, callback);
-    }
+    },
   };
 };
 
@@ -75,27 +78,10 @@ const configureImageLoader = () => ({
     {
       loader: "file-loader",
       options: {
-        name: `${settings.imagesFolder}/[name].[hash].[ext]`
-      }
+        name: `${settings.imagesFolder}/[name].[hash].[ext]`,
+      },
     },
-    {
-      loader: "img-loader",
-      options: {
-        plugins: [
-          require("imagemin-mozjpeg")({
-            quality: 80,
-            progressive: true,
-            arithmetic: false
-          }),
-          require("imagemin-pngquant")({
-            speed: 10,
-            strip: true,
-            quality: [0.3, 0.5]
-          })
-        ]
-      }
-    }
-  ]
+  ],
 });
 
 const configureSVGLoader = () => ({
@@ -111,33 +97,23 @@ const configureSVGLoader = () => ({
         stripdeclarations: true,
         // Inline files smaller than 4 kB
         limit: 4 * 1024,
-        name: `${settings.imagesFolder}/[name].[hash].[ext]`
-      }
+        name: `${settings.imagesFolder}/[name].[hash].[ext]`,
+      },
     },
-    // optimizes/cleanup svg files output from sketch, photoshop etc.
-    {
-      loader: "img-loader",
-      options: {
-        plugins: [
-          require("imagemin-svgo")({
-            plugins: [{ removeTitle: true }, { convertPathData: false }]
-          })
-        ]
-      }
-    }
-  ]
+  ],
 });
 
 const configureCSSLoader = () => ({
   test: /\.scss$/,
+  sideEffects: true,
   use: [
     MiniCssExtractPlugin.loader,
     {
       loader: "css-loader",
       options: {
         importLoaders: 2,
-        modules: { localIdentName: "[hash:base64:4]" }
-      }
+        modules: { localIdentName: "[hash:base64:4]" },
+      },
     },
     {
       loader: "postcss-loader",
@@ -147,14 +123,14 @@ const configureCSSLoader = () => ({
           postcssPresetEnv({
             stage: false, // disable all polyfill; only polyfill stuff in 'features'
             features: { "custom-properties": true },
-            browsers: Object.values(pkg.browserslist.evergreen) // determines which polyfills are required based upon the browsers you are supporting
+            browsers: Object.values(pkg.browserslist.evergreen), // determines which polyfills are required based upon the browsers you are supporting
           }),
-          cssnano() // css minifier. uses 'default' preset
-        ]
-      }
+          cssnano(), // css minifier. uses 'default' preset
+        ],
+      },
     },
-    "sass-loader"
-  ]
+    "sass-loader",
+  ],
 });
 
 module.exports = {
@@ -163,22 +139,23 @@ module.exports = {
     filename: `${settings.jsFolder}/[name].[chunkhash].js`,
     path: settings.outputPath,
     chunkFilename: "[name].[chunkhash].js",
-    publicPath: "/"
+    publicPath: "/",
   },
   optimization: {
     // split webpack runtime/manifest code into a separate chunk
     // so that hashes stay same when rebuilding without changes
     runtimeChunk: "single",
+    usedExports: true,
     splitChunks: {
       chunks: "all",
-      minSize: 0
+      minSize: 0,
     },
     minimizer: [
       new TerserPlugin({
         cache: true,
-        parallel: true
-      })
-    ]
+        parallel: true,
+      }),
+    ],
   },
   module: {
     rules: [
@@ -189,20 +166,25 @@ module.exports = {
       {
         // for third-party library's css that we wanna skip encoding to css-modules
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
-      }
-    ]
+        sideEffects: true,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
   },
   plugins: [
     // always deletes the dist folder first in each prod run
     // default to delete output.path
     new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      { from: "robots.txt" },
+      { from: "src/assets/favicon" },
+    ]),
     // so that file hashes don't change unexpectedly
     new webpack.HashedModuleIdsPlugin(),
     // Extract css into separate .css file
     new MiniCssExtractPlugin({
       filename: "[name].[contenthash].css", // for long-term caching
-      chunkFilename: "[id].[chunkhash].css"
+      chunkFilename: "[id].[chunkhash].css",
     }),
     // make webpack automatically creates index.html with proper hashed
     // style and scripts files for us
@@ -215,19 +197,26 @@ module.exports = {
         collapseWhitespace: true,
         preserveLineBreaks: true,
         minifyURLs: true,
-        removeComments: true
-      }
+        removeComments: true,
+      },
     }),
     new CompressionPlugin(configureCompression()),
     new BundleAnalyzerPlugin({
+      openAnalyzer: false,
       analyzerMode: "static",
-      reportFilename: "report-modern.html"
+      reportFilename: "report-modern.html",
     }),
     new ScriptExtHtmlWebpackPlugin({
       inline: "runtime",
       preload: /\.css$/,
-      defer: /\.js$/
-    })
+      defer: /\.js$/,
+    }),
   ],
-  stats: "minimal"
+  // Stats for tree shaking outcome 
+  stats: {
+    // Examine all modules
+    maxModules: Infinity,
+    // Display bailout reasons
+    optimizationBailout: true,
+  },
 };
